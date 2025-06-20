f236daa49d6ce22107407a6e152d79fcf63f72ec
Gajendra <gajendramalviya1512@gmail.com>
2025-06-20 12:06:11 +0530

    In Server assignment assigned WriteHeader()/ StatusCode to all the handlers

--- Day6/main.go
+++ Day6/main.go
@@ -7,25 +7,22 @@ import (
 	"strconv"
 )
 
-
-// Task 
 type task struct {
 	id     int
 	desc   string
 	status bool
 }
 
-// Slice of task
 type tasks struct {
 	cont []task
 }
 
-// Pointer to the slice 
 type handle struct {
-    ptr * tasks
+	ptr *tasks
 }
 
-// Genereate id number 
+// var t1 tasks
+
 var temp_id = idgen()
 
 func idgen() func() int {
@@ -36,8 +33,7 @@ func idgen() func() int {
 	}
 }
 
-// Returns Task struct of particular id
-func GetTaskById(id int,h *handle) *task {
+func GetTaskById(id int, h *handle) *task {
 
 	for _, val := range h.ptr.cont {
 
@@ -48,27 +44,23 @@ func GetTaskById(id int,h *handle) *task {
 	return nil
 }
 
-// This request show All Tasks
-func (h* handle)handleAllTasks(w http.ResponseWriter, r *http.Request) {
+func (h *handle) handleAllTasks(w http.ResponseWriter, r *http.Request) {
 
-    if r.Method == "GET"{
-
-    w.Write([]byte("List of all tasks :- \n"))
-	for _, val := range h.ptr.cont {
-        str:=fmt.Sprintf("Task %d : %s is done? %v \n",val.id,val.desc,val.status)
-		 w.Write([]byte(str))
+	if r.Method == "GET" {
+		w.Write([]byte("List of all tasks :- \n"))
+		for _, val := range h.ptr.cont {
+			str := fmt.Sprintf("Task %d : %s is done? %v \n", val.id, val.desc, val.status)
+			w.Write([]byte(str))
+		}
 	}
-
-}
 }
 
-// To add Task 
-func (h* handle)handleAddTask(w http.ResponseWriter, r *http.Request) {
+func (h *handle) handleAddTask(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		bodyBy, err := io.ReadAll(r.Body)
 		if err != nil {
-			w.Write([]byte("invalid body"))
 			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("invalid body"))
 			return
 		}
 
@@ -79,14 +71,14 @@ func (h* handle)handleAddTask(w http.ResponseWriter, r *http.Request) {
 		} else {
 			t := task{temp_id(), desc, false}
 			h.ptr.cont = append(h.ptr.cont, t)
-            str:=fmt.Sprintf("Task %s added succesfully with id : %d",desc,t.id)
+			str := fmt.Sprintf("Task %s added succesfully with id : %d", desc, t.id)
+			w.WriteHeader(http.StatusCreated)
 			w.Write([]byte(str))
 		}
 	}
 }
 
-// Get particular task by id
-func (h * handle)handleGetTaskById(w http.ResponseWriter, r *http.Request) {
+func (h *handle) handleGetTaskById(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "GET" {
 
@@ -95,79 +87,73 @@ func (h * handle)handleGetTaskById(w http.ResponseWriter, r *http.Request) {
 
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-            return
+			w.Write([]byte(`invalid id`))
+			return
 		}
 
-		t := GetTaskById(id,h)
-        str:=fmt.Sprintf("Task of id : %d is %s",id,t.desc)
+		t := GetTaskById(id, h)
+		str := fmt.Sprintf("Task of id : %d is %s", id, t.desc)
 		w.Write([]byte(str))
-
 	}
 }
 
+func (h *handle) handleCompleteTask(w http.ResponseWriter, r *http.Request) {
 
-// To complete particular task
-func (h * handle) handleCompleteTask(w http.ResponseWriter, r *http.Request){
+	if r.Method == "PUT" {
 
-    if r.Method=="PUT" {
-
-        idstr := r.PathValue("id")
+		idstr := r.PathValue("id")
 		id, err := strconv.Atoi(idstr)
 
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-            return
+			return
 		}
-        
 
-        if(h.ptr.cont[id].status) {
-          w.Write([]byte("Task is already Completed"))
-        }else {
-            h.ptr.cont[id].status=true
-            str:=fmt.Sprintf("Task %d is completed ",id)
-            w.Write([]byte(str))
+		if h.ptr.cont[id].status {
+			w.Write([]byte("Task is already Completed"))
+		} else {
+			h.ptr.cont[id].status = true
+			str := fmt.Sprintf("Task %d is completed ", id)
+			w.Write([]byte(str))
 
-        }
-        
-    }
-}
+		}
 
-// List all the pending tasks
-func (h* handle)handlePendingTasks(w http.ResponseWriter, r *http.Request) {
-    
-    if r.Method =="GET" {
-    w.Write([]byte("List of Pending tasks :- \n"))
-	for _, val := range h.ptr.cont {
-        if(!val.status){
-        str:=fmt.Sprintf("Task %d : %s is Not completed yet \n",val.id,val.desc)
-		 w.Write([]byte(str))
-        } 
 	}
-
- }
 }
 
+func (h *handle) handlePendingTasks(w http.ResponseWriter, r *http.Request) {
 
-// Main Function
+	if r.Method == "GET" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("List of Pending tasks :- \n"))
+		for _, val := range h.ptr.cont {
+			if !val.status {
+				str := fmt.Sprintf("Task %d : %s is Not completed yet \n", val.id, val.desc)
+				w.Write([]byte(str))
+			}
+		}
+
+	}
+}
+
 func main() {
 
 	a1 := task{temp_id(), " Grocery ", false}
 	a2 := task{temp_id(), " Shopping ", false}
-    
-    var h handle
 
-    h.ptr=&tasks{}
-    
+	var h handle
+
+	h.ptr = &tasks{}
+
 	h.ptr.cont = append(h.ptr.cont, a1, a2)
 
 	http.HandleFunc("/showall", h.handleAllTasks)
 	http.HandleFunc("/task/{id}", h.handleGetTaskById)
 	http.HandleFunc("/add", h.handleAddTask)
-    http.HandleFunc("/showpending", h.handlePendingTasks)
-    http.HandleFunc("/do/{id}", h.handleCompleteTask)
+	http.HandleFunc("/showpending", h.handlePendingTasks)
+	http.HandleFunc("/do/{id}", h.handleCompleteTask)
 
-
-	fmt.Println("Server started ")
+	fmt.Println("Server Started running on http://localhost:8000/")
 	http.ListenAndServe(":8000", nil)
 
 }
